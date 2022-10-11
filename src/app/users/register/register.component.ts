0cd873c50ddd26685c1f9f0af3c5cf8ae9333079
Leya Kunjumon <leyakunjumon99@gmail.com>
2022-10-11 18:21:55 +0530

    validation4

--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
   register:any = FormGroup;
-  user_list:any[] = [] ;
+  user_list: any = [] ;
   dtt:any ;
   constructor(private fb:FormBuilder,private router:Router) { }
 
@@ -19,44 +19,86 @@ export class RegisterComponent implements OnInit {
       pswd:['',Validators.required]
 
       });
-       this.dtt = localStorage.getItem('name');
-       this.user_list = JSON.parse(this.dtt);
-  }
-  loginData :any;
-  data :any;
-  data1 :any;
-  data2:any;
-  data3: any;
+      if (localStorage.getItem('name') != null ){
+
+        this.dtt = localStorage.getItem('name');
+        this.user_list.push(JSON.parse(this.dtt));
+
+       }
+      // localStorage.setItem('name',JSON.stringify(this.user_list))
+  };
+  
   uname :any;
   umail :any;
   val1 :any;
-  i:any;
-  signupsubmit(){
-    console.log('hlo');
-    this.loginData = localStorage.getItem('name');
-    this.data = JSON.parse(this.loginData)
-    if ( localStorage.getItem('name') == null ){
-      console.log('hlo')
-      this.user_list.push(this.register.value);
-      console.log(this.user_list);
-      localStorage.setItem("name",JSON.stringify(this.user_list));
-      this.router.navigate(['login']);
-    }
-    else if (localStorage.getItem('name') != null ){
-      for (let i=0;i<this.data.length;i++){
-       if (this.data[i].email != this.umail) {
-        this.user_list.push(this.register.value);
-        console.log(this.user_list);
-        localStorage.setItem("name",JSON.stringify(this.user_list));
-        this.router.navigate(['login']);
-    } 
-    else{
-    this.val1 = 'username and email already taken.try another!'
-    this.router.navigate(['register']);
-  }
-}
-}
-}
-  
-}
+  ln:any;
+  ntng :any;
 
+  signupsubmit(){
+    // this.dtt = localStorage.getItem('name');
+    // this.user_list.push(JSON.parse(this.dtt));
+    this.user_list.push(this.register.value)
+    localStorage.setItem("name",JSON.stringify(this.user_list));
+    this.router.navigate(['login']);
+    // if ( localStorage.getItem('name') == null ){
+     
+    // }
+
+    if (localStorage.getItem('name') != null ){
+      
+      for (let i=0;i<this.user_list.length;i++){
+          
+          if (this.user_list[i].email != this.umail) {
+             console.log('hlo');
+             
+         
+         
+          }
+          else {
+            console.log('kkk')
+          }
+   }
+         }
+        }
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // };
+      // }
+//     else {
+//   this.ntng ='nothing';
+//  }
+ 
+//  }
+ //       console.log('mm')
+    //       this.user_list.push(this.register.value);
+    //       console.log(this.user_list)
+    //       localStorage.setItem("name",JSON.stringify(this.user_list));
+    //       this.router.navigate(['login']);
+
+
+ //     else {
+    //       console.log('kk')
+    //       // this.val1 = 'username and email already taken.try another!'
+    //       this.router.navigate(['register']);
+         
